68052a74a7156812fea5c6bda052edb7c6792715
Christopher Angelo <angelo@angeloanan.xyz>
2024-11-04 21:00:12 +0700

    feat(printer): alter default print layout & make trait pub

--- src/printer.rs
+++ src/printer.rs
@@ -11,7 +11,7 @@ pub const JUSTIFY_LEFT: &[u8; 3] = &[ESC, b'a', 0x0];
 pub const JUSTIFY_CENTER: &[u8; 3] = &[ESC, b'a', 0x1];
 pub const JUSTIFY_RIGHT: &[u8; 3] = &[ESC, b'a', 0x2];
 
-trait Printable {
+pub trait Printable {
     fn into_print_data(&self) -> Vec<u8>;
 }
 
@@ -26,6 +26,7 @@ pub struct PrintData {
 impl Printable for PrintData {
     fn into_print_data(&self) -> Vec<u8> {
         let mut out: Vec<u8> = vec![ESC, b'@']; // Initialize print
+        out.extend_from_slice(&[GS, b'b', 0x01]); // Enable font smoothing
         out.extend_from_slice(&[ESC, b'M', 0x01]); // Uses smaller character font
 
         // Extend_from_slice might just slowing things down too much
@@ -34,6 +35,8 @@ impl Printable for PrintData {
         out.extend_from_slice(self.title.as_bytes()); // Send title
         out.extend_from_slice(&[LF]); // Print
 
+        out.extend_from_slice(&[ESC, b'd', 0x01]); // Feed lines
+        out.extend_from_slice(&[ESC, b'M', 0x00]); // Uses default character font
         out.extend_from_slice(&[GS, b'!', 0x00]); // Set character size to 1x1
         out.extend_from_slice(JUSTIFY_LEFT); // Set justify left
 
@@ -43,7 +46,7 @@ impl Printable for PrintData {
             out.extend_from_slice(subtitle.as_bytes()); // Send subtitle
             out.extend_from_slice(&[LF]); // Print
 
-            out.extend_from_slice([b'-'].repeat(64).as_slice()); // Send line
+            out.extend_from_slice([b'-'].repeat(48).as_slice()); // Send line
             out.extend_from_slice(&[LF]); // Print
         }
 
@@ -66,7 +69,8 @@ impl Printable for PrintData {
 
         // Print timestamp
         let human_time = self.timestamp.format("%B %e, %r");
-        let timestamp_line = format!("Created at: {human_time}");
+        out.extend_from_slice(&[ESC, b'd', 0x02]); // Feed 2 lines
+        let timestamp_line = format!("Timestamp: {human_time}");
         out.extend_from_slice(timestamp_line.as_bytes()); // Send timestamp_line
         out.extend_from_slice(&[LF]); // Print timestamp
 
@@ -92,8 +96,8 @@ pub async fn process_prints(
 
                 // Closing
                 printer.write_all(&[ESC, b'd', 0x06, LF]).await.unwrap(); // Feed 6 lines
-                printer.write_all(&[ESC, b'i']).await.unwrap(); // Full cut; I think the auto cutter is 2 lines behind(?), so the line above effectively feeds 4 line
-                printer.write_all(&[0x0C]).await.unwrap(); //Print and return to standard mode in page mode
+                printer.write_all(&[ESC, b'i']).await.unwrap(); // Full cut; I think the auto cutter is 2 lines(?) behind, so the line above effectively feeds 4 line
+                printer.write_all(&[0x0C]).await.unwrap(); // Print and return to standard mode in page mode; Finishes the job
             }
         }
     }
